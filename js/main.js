5ec664a2dbc979f737263d408d0eaa339ce400b6
saniok333 <alex.o.csc@gmail.com>
2019-11-30 00:39:18 +0200

    hw

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(() => {
+    $('body').on('submit', '#searchForm', (e) => {
+        let searchText = $('#searchText').val();
+        let searchType = $('#searchType').val();
+        e.preventDefault();
+        getMovies(searchText, searchType, 1);
+    });
+    $('body').on('click', '.pagin-btn', (e) => {
+        let searchText = $('#searchText').val();
+        let searchType = $('#searchType').val();
+        e.preventDefault();
+        getMovies(searchText, searchType, e.target.textContent);
+    });
+});
+
+let getMovies = (searchText, searchType, pageNumb) => {
+    $('#details').html('');
+    let request = `https://www.omdbapi.com/?apikey=68c6b1ea&s=${searchText}&type=${searchType}&page=${pageNumb}`;
+    fetch(request).then((result) => {
+        return result.json();
+    }).then((data) => {
+        let pagesAmount = Math.ceil(data.totalResults / 10);
+        let movies = data.Search;
+        console.log(movies);
+        let output = '';
+        $.each(movies, (index, movie) => {
+            output += `<p>${(pageNumb-1)*10+index+1}. ${movie.Title} 
+            <button 
+            onclick="showDetails('${movie.Title}', '${movie.Year}', '${movie.Poster}')">
+            Details...</button></p>`;
+        });
+        if (pagesAmount > 1) output += formPaginBtnsSection(pageNumb, pagesAmount);
+        $('#movies').html(output);
+        $(`#pagin_btn${pageNumb}`).attr('disabled', 'disabled');
+    });
+};
+
+let formPaginBtnsSection = (pageNumb, pagesAmount) => {
+    let tempArr = [];
+    for (let i = (+pageNumb - 5); i <= (+pageNumb + 5); i++) {
+        if (i > 1 && i < pagesAmount) {
+            tempArr.push(i);
+        };
+    };
+    let paginationSection = `<button class="pagin-btn" id="pagin_btn1">1</button>`;
+    if (tempArr[0] > 2) {
+        paginationSection += `<span>. . .<span>`;
+    };
+    for (let btnNumb of tempArr) {
+        paginationSection += `<button class="pagin-btn" id="pagin_btn${btnNumb}">${btnNumb}</button>`;
+    };
+    if (tempArr[tempArr.length - 1] < (pagesAmount - 1)) {
+        paginationSection += `<span>. . .<span>`;
+    };
+    paginationSection += `<button class="pagin-btn" id="pagin_btn${pagesAmount}">${pagesAmount}</button>`;
+    return paginationSection;
+};
+
+let showDetails = (tite, year, poster) => {
+    movieDetails = `<h3>${tite}</h3><h4>${year}</h4><img src="${poster}"></img>`;
+    $('#details').html(movieDetails);
+};
